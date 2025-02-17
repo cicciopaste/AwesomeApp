31bbe149629aa800bde333703fa0c3f3de9f628a
fecandia <federico.candia@modis.com>
2025-02-17 10:15:20 +0100

    startup project

---
+++ src/main/java/org/example/service/BookingController.java
@@ -0,0 +1,22 @@
+package org.example.service;
+
+import lombok.Data;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.jdbc.core.namedparam.MapSqlParameterSource;
+import org.springframework.jdbc.core.namedparam.NamedParameterJdbcTemplate;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.bind.annotation.RestController;
+
+import static org.springframework.web.bind.annotation.RequestMethod.*;
+
+@RestController
+@RequestMapping("/booking")
+public class BookingController {
+
+	@GetMapping("/{id}")
+	 public String readBooking (@PathVariable("id") Long bookingCode){
+		 return "";
+	 }
+}
